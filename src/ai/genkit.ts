a1e6b46a8364a5d86077225ff0bb133de85805b4
Jayesh singh <jayeshkumarsingh11@gmail.com>
2025-07-21 13:41:41 +0000

    use the latest version of the gemini

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,14 +1,15 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 import { gemini15Flash, gemini15Pro } from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: gemini15Pro,
+  model: 'googleai/gemini-1.5-pro-latest',
   embedder: 'googleai/text-embedding-004',
   textToSpeech: 'googleai/gemini-2.5-flash-preview-tts',
   imageGenerator: 'googleai/gemini-2.0-flash-preview-image-generation'
 });
 
-export const fastModel = gemini15Flash;
-export const proModel = gemini15Pro;
+export const fastModel = 'googleai/gemini-1.5-flash-latest';
+export const proModel = 'googleai/gemini-1.5-pro-latest';
