83b73a20bc2d522898d52dcabe26697d212c0fad
Kritika Singh <kritikasince05@gmail.com>
2025-07-24 16:42:50 +0000

    fix this error

--- src/components/SamriddhKhetiApp.tsx
+++ src/components/SamriddhKhetiApp.tsx
@@ -8,7 +8,6 @@ import IrrigationPlanner from "@/components/IrrigationPlanner"
 import CropAdvisor from "@/components/CropAdvisor"
 import SoilQualityAdvisor from "@/components/SoilQualityAdvisor"
 import SchemeFinder from "@/components/SchemeFinder"
-import TopNavBar from './TopNavBar'
 
 export type NavItem = "Dashboard" | "Irrigation Planner" | "Crop Advisor" | "Soil Advisor" | "Govt. Schemes" | "Home" | "About Us" | "Contact Us" | "Tools";
 
@@ -52,11 +51,8 @@ export default function SamriddhKhetiApp({ initialView = "Dashboard", onNavigate
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-background">
-      <TopNavBar activeItem={activeView} setActiveItem={handleNavigation} />
-      <main className="flex-1 p-4 lg:p-6">
-        {renderContent()}
-      </main>
-    </div>
+    <main className="flex-1 p-4 lg:p-6">
+      {renderContent()}
+    </main>
   )
 }
